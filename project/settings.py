cb04273c21833fffb5eba249161ad0f3f4e963c5
kenanwahbeh <kinan.wehba@gmail.com>
2024-06-14 20:05:37 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   accounts/__pycache__/__init__.cpython-312.pyc
    	new file:   accounts/__pycache__/admin.cpython-312.pyc
    	new file:   accounts/__pycache__/apps.cpython-312.pyc
    	new file:   accounts/__pycache__/models.cpython-312.pyc
    	new file:   accounts/migrations/__pycache__/__init__.cpython-312.pyc
    	new file:   blog/__pycache__/__init__.cpython-312.pyc
    	new file:   blog/__pycache__/admin.cpython-312.pyc
    	new file:   blog/__pycache__/apps.cpython-312.pyc
    	new file:   blog/__pycache__/models.cpython-312.pyc
    	new file:   blog/migrations/__pycache__/__init__.cpython-312.pyc
    	new file:   contact/__pycache__/__init__.cpython-312.pyc
    	new file:   contact/__pycache__/admin.cpython-312.pyc
    	new file:   contact/__pycache__/apps.cpython-312.pyc
    	new file:   contact/__pycache__/models.cpython-312.pyc
    	new file:   contact/migrations/__pycache__/__init__.cpython-312.pyc
    	modified:   db.sqlite3
    	new file:   home/__pycache__/__init__.cpython-312.pyc
    	new file:   home/__pycache__/admin.cpython-312.pyc
    	new file:   home/__pycache__/apps.cpython-312.pyc
    	new file:   home/__pycache__/models.cpython-312.pyc
    	new file:   home/migrations/__pycache__/__init__.cpython-312.pyc
    	new file:   job/__pycache__/__init__.cpython-312.pyc
    	new file:   job/__pycache__/admin.cpython-312.pyc
    	new file:   job/__pycache__/apps.cpython-312.pyc
    	new file:   job/__pycache__/models.cpython-312.pyc
    	new file:   job/migrations/__pycache__/__init__.cpython-312.pyc
    	modified:   project/__pycache__/settings.cpython-312.pyc
    	modified:   project/settings.py

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #my_apps
+    'job',
+    'accounts',
+    'blog',
+    'contact',
+    'home',
+    
+
 ]
 
 MIDDLEWARE = [
